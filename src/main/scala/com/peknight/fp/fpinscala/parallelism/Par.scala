aec826c70e3e3b0ef3f50ba8634cf96ff592f12a
peknight <JKpeknight@gmail.com>
2019-10-07 22:14:10 +0800

    update parallelism

--- src/main/scala/com/peknight/fp/fpinscala/parallelism/Par.scala
+++ src/main/scala/com/peknight/fp/fpinscala/parallelism/Par.scala
@@ -66,6 +66,39 @@ object Par {
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = ???
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
+  def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
+
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] = map2(pa, unit(()))((a, _) => f(a))
+
+  def sequence_simple[A](l: List[Par[A]]): Par[List[A]] = l.foldRight[Par[List[A]]](unit(List()))((h, t) =>
+    map2(h, t)(_ :: _)
+  )
+
+  def sequenceRight[A](as: List[Par[A]]): Par[List[A]] = as match {
+    case Nil => unit(Nil)
+    case h :: t => map2(h, fork(sequenceRight(t)))(_ :: _)
+  }
+
+  def sequenceBalanced[A](as: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = fork {
+    if (as.isEmpty) unit(Vector())
+    else if (as.length == 1) map(as.head)(a => Vector(a))
+    else {
+      val (l, r) = as.splitAt(as.length / 2)
+      map2(sequenceBalanced(l), sequenceBalanced(r))(_ ++ _)
+    }
+  }
+
+  def sequence[A](as: List[Par[A]]): Par[List[A]] = map(sequenceBalanced(as.toIndexedSeq))(_.toList)
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+    val pars: List[Par[List[A]]] = as.map (asyncF((a: A) => if (f(a)) List(a) else List()))
+    map(sequence(pars))(_.flatten)
+  }
 }
 object ParTest extends App {
   Par.run(new ThreadPoolExecutor(5, 5, 60, TimeUnit.SECONDS,
